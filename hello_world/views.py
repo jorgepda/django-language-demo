3030ea0bff8d21bf60df66a823ce0b4aa9cea3ec
Jorge Perez de Acha <perezde@cshl.edu>
2018-03-30 15:36:47 -0400

    Added translatable form.

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,7 +1,19 @@
 # encoding: utf-8
 from django.utils.translation import ugettext as _
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+
+from .forms import TeacherTrainingForm
 
 def index(request):
-    return render(request, 'hello_world/index.html')
+    if request.method == 'POST':
+        form = TeacherTrainingForm(request.POST)
+        if form.is_valid():
+            # process form here
+            return HttpResponseRedirect('/thanks/')
+    else:
+        form = TeacherTrainingForm()
+
+    return render(request, 'hello_world/index.html', {'form': form})
+
+#def get_teacher_training_form(request):
