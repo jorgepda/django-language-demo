afa3b01c495e47509546aa492fe9cfa388f1e3aa
Jorge Perez de Acha <perezde@cshl.edu>
2018-04-03 11:11:37 -0400

    Displays translated messages and error messages

--- hello_world/forms.py
+++ hello_world/forms.py
@@ -2,10 +2,10 @@ from django import forms
 from django.utils.translation import ugettext_lazy as _
 
 class TeacherTrainingForm(forms.Form):
-    first_name = forms.CharField(label=_("First name"), max_length=100)
-    last_name = forms.CharField(label=_("Last name"), max_length=100)
-    email = forms.EmailField(label=_("Email address"))
-    school_name = forms.CharField(label=_("School name"), max_length=250)
-    school_district = forms.CharField(label=_("School district"), max_length=250)
+    first_name = forms.CharField(label=_("First name"), max_length=100, required=False)
+    last_name = forms.CharField(label=_("Last name"), max_length=100, required=False)
+    email = forms.EmailField(label=_("Email address"), required=False)
+    school_name = forms.CharField(label=_("School name"), max_length=250, required=False)
+    school_district = forms.CharField(label=_("School district"), max_length=250, required=False)
     school_choices = (("public", _("Public School")),("private", _("Private or Charter School")),("other", _("Other")),)
     school_type = forms.ChoiceField(label=_("School type"), choices=school_choices)
--- hello_world/views.py
+++ hello_world/views.py
@@ -2,6 +2,7 @@
 from django.utils.translation import ugettext as _
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.contrib import messages
 
 from .forms import TeacherTrainingForm
 
@@ -10,10 +11,16 @@ def index(request):
         form = TeacherTrainingForm(request.POST)
         if form.is_valid():
             # process form here
-            return HttpResponseRedirect('/thanks/')
+	    first_name = form.cleaned_data['first_name']
+	    if (len(first_name) < 3):
+	        messages.error(request, _("Your first name is too short"))
+	    else:
+                messages.success(request, _("Thank you for submitting your form"))
+            return render(request, 'hello_world/index.html', {'form': form})
+        else:
+            messages.success(request, _("There was an error while submitting your form"))
     else:
         form = TeacherTrainingForm()
 
     return render(request, 'hello_world/index.html', {'form': form})
 
-#def get_teacher_training_form(request):
